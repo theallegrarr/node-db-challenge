7eae48b2615c98c964e6b93149b6419679f06b55
theallegrarr <nurudeen.amedu@gmail.com>
2019-11-08 09:28:33 +0100

    migrations created

---
+++ data/migrations/20191108091548_project_migrations.js
@@ -0,0 +1,29 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('projects', table => {
+    table.increments();
+    table.string('name').notNullable();
+    table.string('description').notNullable();
+    table.boolean('completed').notNullable().defaultTo(false);
+  }).createTable('tasks', table => {
+    table.increments();
+    table.string('name').notNullable();
+    table.string('description').defaultTo('none provided');
+    table.integer('project_id').references('id').inTable('projects');
+  }).createTable('resources', table => {
+    table.increments();
+    table.string('name').notNullable();
+    table.string('description').defaultTo('none provided');
+  }).createTable('projects_resources', table => {
+    table.increments();
+    table.integer('project_id').notNullable().references('id').inTable('projects');
+    table.integer('resource_id').notNullable().references('id').inTable('resources');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('projects_resources')
+                    .dropTableIfExists('tasks')
+                    .dropTableIfExists('resources')
+                    .dropTableIfExists('projects');
+};
